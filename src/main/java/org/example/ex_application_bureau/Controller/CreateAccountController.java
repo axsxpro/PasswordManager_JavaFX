65201bf8d9017a075a7ab05693a2849e9fad88bb
axsxpro <cmptpro@outlook.com>
2024-04-03 05:10:02 +0200

    maj label

--- src/main/java/org/example/ex_application_bureau/Controller/CreateAccountController.java
+++ src/main/java/org/example/ex_application_bureau/Controller/CreateAccountController.java
@@ -1,6 +1,66 @@
 package org.example.ex_application_bureau.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class CreateAccountController {
+
+    @FXML
+    private Button button_reset;
+
+    @FXML
+    private Button create_Button;
+
+    @FXML
+    private TextField field_email;
+
+    @FXML
+    private PasswordField field_password;
+
+    @FXML
+    private Label label_error;
+
+
+
+
+    public void addUser () {
+
+
+
+    }
+
+
+
+
+
+    @FXML
+    void createUser(ActionEvent event) {
+
+        if ((field_email.getText().isBlank()) || field_password.getText().isBlank()) {
+
+            label_error.setText("Field cannot be blank");
+
+        } else {
+
+            addUser();
+        }
+
+    }
+
+
+    @FXML
+    void resetForm(ActionEvent event) {
+
+        // Réinitialiser les champs du formulaire
+        field_email.clear();
+        field_password.clear();
+        label_error.setText("");
+
+    }
 
 
 
